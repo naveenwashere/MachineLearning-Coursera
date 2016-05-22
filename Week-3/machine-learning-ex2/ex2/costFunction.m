6644f20d6e898ce5360bd8be122e9cafa39910ad
Naveen Kumar Kalapati <naveenwashere@gmail.com>
2016-05-22 11:38:54 +0530

    Correcting the logistic regression cost function formula

--- Week-3/machine-learning-ex2/ex2/costFunction.m
+++ Week-3/machine-learning-ex2/ex2/costFunction.m
@@ -20,7 +20,7 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-J = (1 / m) * sum((-y' * log(sigmoid(X * theta))) - ((1 - y)' * log(1 - sigmoid(X * theta))));
+J = (1 / m) * sum((-y .* log(sigmoid(X * theta))) - ((1 - y) .* log(1 - sigmoid(X * theta))));
 
 grad = (1 / m) * ((X' * sigmoid(X * theta)) - (X' * y));
 
