c72929a6107c3b3d40139f62b019a5b0a4362614
Naveen Kumar Kalapati <naveenwashere@gmail.com>
2016-06-04 21:15:09 +0530

    Solutions for first two exercises

--- Week-7/machine-learning-ex6/ex6/dataset3Params.m
+++ Week-7/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,28 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+CArray = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmaArray = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+cLen = size(CArray, 2);
+sigmaLen = size(sigmaArray, 2);
 
+predError = 999999;
 
+%model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(X, X, sigma);
 
-
+for i = 1 : cLen,
+	for j = 1 : sigmaLen,
+		model = svmTrain(X, y, CArray(i), @(x1, x2) gaussianKernel(x1, x2, sigmaArray(j)));
+		predictions = svmPredict(model, Xval);
+		newPredError = mean(double(predictions ~= yval));
+		if (newPredError < predError)
+			predError = newPredError;
+			C = CArray(i);
+			sigma = sigmaArray(j);
+		endif;
+	end;
+end;
 
 % =========================================================================
 
